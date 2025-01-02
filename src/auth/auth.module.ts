5aadbbdfb70d5955e01d00774cd3cdee8285fc04
seungho-jg <tmdgh971229@gmail.com>
2025-01-02 11:38:27 +0900

    Feat: Like 기능 구현 완료

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { UsersModule } from 'src/users/users.module';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h'},
+      signOptions: { expiresIn: '7d'},
     })
   , PrismaModule],
   controllers: [AuthController],
